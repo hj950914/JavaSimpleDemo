c2020df861b68be4137079731136b535d5910c18
huangjie <15907081130@163.com>
2019-04-03 16:32:52 +0800

    new

---
+++ src/main/java/com/hj/Demo3/LombokUser.java
@@ -0,0 +1,17 @@
+package com.hj.Demo3;
+
+import lombok.Data;
+
+/**
+ * Author: hj
+ * Date: 2019-04-03 15:58
+ * Description: <lombok使用--简化javabean>
+ */
+@Data
+public class LombokUser {
+
+    //用户名
+    private String username;
+    //密码
+    private String password;
+}
---
+++ src/main/java/com/hj/Demo3/LombokUserTest.java
@@ -0,0 +1,26 @@
+package com.hj.Demo3;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Author: hj
+ * Date: 2019-04-03 16:12
+ * Description: <lombok测试>
+ */
+public class LombokUserTest {
+
+    public static void main(String[] args) {
+        LombokUser lombokUser = new LombokUser();
+        lombokUser.setUsername("hj");
+        lombokUser.setPassword("123");
+        System.out.println(lombokUser);
+    }
+
+    @Test
+    void testLombok() {
+        LombokUser lombokUser = new LombokUser();
+        lombokUser.setUsername("hj");
+        lombokUser.setPassword("123");
+        System.out.println(lombokUser);
+    }
+}
